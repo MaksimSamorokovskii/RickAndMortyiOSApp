7131f50b768dcd40d2159a2aa8f42232d90f8b08
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-09-21 17:12:04 +0700

    Added Models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Максим Самороковский on 21.09.2024.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Максим Самороковский on 21.09.2024.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+      let name: String
+      let url: String
+}
